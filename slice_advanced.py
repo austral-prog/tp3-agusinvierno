c8d9db34bef1dbfafa832d22e95e176a9ae02f4a
agusinvierno <agustinainvierno@gmail.com>
2025-08-25 10:51:01 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,11 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    texto = input()
+
+    print(texto[4: :2])
+
+slice_advanced()
+
+# Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,13 @@
 def slice_simple():
     texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    print(texto.lower()[0:3])
+    print(texto[2:5])
+    print(texto.lower())
 
+
+slice_simple()
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
